4493944d66b00fb8d5e8b6ca824767d27d1ff1d6
Lauren Greene <laurengreene@arizona.edu>
2025-02-12 16:54:08 -0700

    Add files via upload

---
+++ la1_labby/test/music/MusicStoreTest.java
@@ -0,0 +1,14 @@
+package music;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MusicStoreTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
