6726031f373ed9a07994cb0b9ca212c59d190e34
Lauren Greene <laurengreene@arizona.edu>
2025-02-24 01:52:51 -0700

    Update TestAlbum.java
    added more tests

--- la1_labby/test/music/TestAlbum.java
+++ la1_labby/test/music/TestAlbum.java
@@ -32,7 +32,41 @@ class TestAlbum {
 	@Test
 	void testAddToSongList() {
 		album.addToSongList(s);
-		assertTrue(album.getSongList().containsSong(s));
+		assertTrue(album.getSongList().get(0).equals(s));
 	}
-
+	
+	@Test
+	void testMakeCopyAlbum() {
+		album.addToSongList(s);
+		Album cAlbum = album.makeCopyAlbum();
+		assertNotSame(album, cAlbum);
+		assertTrue(album.equals(cAlbum));
+	}
+	
+	@Test
+	void testEquals() {
+		Album eAlbum = new Album("Adele", "25", "Pop", "2015");
+		assertTrue(album.equals(eAlbum));
+		
+	}
+	@Test
+	void testNotEquals() {
+		Album difYearAlbum = new Album("Adele", "25", "Pop", "2014");
+		assertFalse(album.equals(difYearAlbum));
+		Album difGenreAlbum = new Album("Adele", "25", "Rap", "2015");
+		assertFalse(album.equals(difGenreAlbum));
+		Album difTitleAlbum = new Album("Adele", "26", "Pop", "2015");
+		assertFalse(album.equals(difTitleAlbum));
+		Album difArtistAlbum = new Album("Coldplay", "25", "Pop", "2015");
+		assertFalse(album.equals(difArtistAlbum));
+	}
+	
+	@Test
+	void testToString() {
+		album.addToSongList(s);
+		String str = "Album: " + album.getTitle() + "\nArtist: " + 
+				album.getArtist() + "\nYear: " + album.getYear() + "\nGenre: " +
+				album.getGenre() + "\nSongs: \n" + s.getTitle();
+		assertEquals(str, album.toString());
+	}
 }
