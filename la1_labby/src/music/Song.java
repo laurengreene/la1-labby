c3f81c19f5ab47803a69d08fe7396df6987d4711
Lauren Greene <laurengreene@arizona.edu>
2025-02-24 01:45:00 -0700

    Update Song.java
    new methods to fix escaping references

--- la1_labby/src/music/Song.java
+++ la1_labby/src/music/Song.java
@@ -3,7 +3,7 @@ package music;
 public class Song {
 
 	// instance variables to add (if needed):
-	// rating
+	// rating (as int or enum?)
 	// don't need boolean for favorite. can determine in method based on rating
 	
 	// methods to add:
@@ -36,9 +36,24 @@ public class Song {
 		return albumTitle;
 	}
 	
+	// return copy of song
+	public Song makeCopy() {
+		Song cSong = new Song(title, artist, albumTitle);
+		return cSong;
+	}
+		
+	public boolean equals(Song otherSong) {
+		if(otherSong.getAlbumTitle().equals(albumTitle) &&
+				otherSong.getArtist().equals(artist) &&
+				otherSong.getTitle().equals(title)) return true;
+		return false;
+	}
+
+	
 	public String toString() {
 		return "Song: " + this.title + "\nArtist: " + this.artist +
 				"\nAlbum: " + this.albumTitle;
 	}
-
+	
+	
 }
