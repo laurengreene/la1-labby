8517b054997c55a7de5dd7cc1df2866adfcb630e
Lauren Greene <laurengreene@arizona.edu>
2025-02-27 00:50:04 -0700

    Update View.java
    added more methods to view

--- la1_labby/src/music/View.java
+++ la1_labby/src/music/View.java
@@ -2,6 +2,7 @@
 package music;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class View {
@@ -86,21 +87,29 @@ public class View {
 		System.out.println("Input Name:");
 		String name = scn.nextLine();
 		checkIfDone(name);
+		ArrayList<Album> albums;
 		switch (tOra) {
 		case "a" :
-			// get album by artist from store
+			albums = mStore.searchStoreAlbumByArtist(name);
 		case "t" :
-			// get album by title from store
+			albums = mStore.searchStoreAlbumByTitle(name);
 		default :
 			System.out.println("Invalid Input");
 			searchStoreForAlbum();
+			albums = new ArrayList<Album>();
 		}
-		// save album information
-		// print album information or 'album not in store'
+		if(albums.size() == 0) System.out.println("Album not found"); searchStoreForAlbum();
+		for (Album a : albums) {
+			System.out.println(a);
+		}
 		System.out.println("Add album to library? (y)/(n)");
 		String ifAdd = scn.nextLine();
 		checkIfDone(ifAdd);
-		if(ifAdd.equals("y")) libModel.addAlbumToLib(null); // change null to album information
+		if(ifAdd.equals("y")) {
+			for(Album a : albums) {
+				libModel.addAlbumToLib(a); 
+			}
+		} 
 		start();
 	}
 	
@@ -111,21 +120,28 @@ public class View {
 		System.out.println("Input Name:");
 		String name = scn.nextLine();
 		checkIfDone(name);
+		ArrayList<Song> songs;
 		switch (tOra) {
 		case "a" :
-			// get song by artist from store
+			songs = mStore.searchStoreSongByArtist(name);
 		case "t" :
-			// get song by title from store
+			songs = mStore.searchStoreSongByTitle(name);
 		default :
 			System.out.println("Invalid Input");
 			searchStoreForSong();
+			songs = new ArrayList<Song>();
 		}
-		// save song information
-		// print song information or 'song not in store'
+		if(songs.size() == 0) System.out.println("Song not found"); searchStoreForSong();
+		for (Song s : songs) {
+			System.out.println(s);
+		}
 		System.out.println("Add song to library? (y)/(n)");
 		String ifAdd = scn.nextLine();
 		checkIfDone(ifAdd);
-		if(ifAdd.equals("y")) libModel.addSongToLib(null); // change null to song information
+		if(ifAdd.equals("y")) {
+			for(Song s : songs)
+			libModel.addSongToLib(s); 
+		}
 		start();
 	}
 	
@@ -137,7 +153,7 @@ public class View {
 		case "s" :
 			getOne();
 		case "a" :
-			getAll();
+			getAll(); // to return list of all objects
 		default :
 			System.out.println("Invalid Input");
 			searchLib();
@@ -162,7 +178,79 @@ public class View {
 	}
 	
 	private void searchLibForSong() {
-		
+		// im not sure how we wanna do this one, we need to keep the 
+		// song reference to update the rating if needed 
+		// maybe use equals method and look through all objects in
+		// songlist and update the one that is equal, could be easier way
+	}
+	
+	private void searchLibForAlbum() {
+		System.out.println("Search by Artist(a) or Title(t)?");
+		String tOra = scn.nextLine();
+		checkIfDone(tOra);
+		System.out.println("Input Name:");
+		String name = scn.nextLine();
+		checkIfDone(name);
+		switch (tOra) {
+		case "a" :
+			System.out.println(libModel.getLibAlbumByArtist(name));
+		case "t" :
+			System.out.println(libModel.getLibAlbumByTitle(name));
+		default :
+			System.out.println("Invalid Input");
+			searchLibForAlbum();
+		}
+	}
+	
+	private void searchForPlaylist() {
+		System.out.println("Playlist name: ");
+		String name = scn.nextLine();
+		checkIfDone(name);
+		String playlist = libModel.getPlaylist(name);
+		if(playlist == "Not Found") {
+			System.out.println("Playlist Not Found"); 
+			searchForPlaylist(); }
+		System.out.println(playlist);
+		System.out.println("Edit Playlist? (y)/(n)");
+		String ifEdit = scn.nextLine();
+		checkIfDone(ifEdit);
+		if (ifEdit.equals("y")) editPlaylist(name);
+		else start();
+	}
+	
+	private void editPlaylist(String pName) {
+		System.out.println("Add(a) or Remove(r) song?");
+		String input = scn.nextLine();
+		checkIfDone(input);
+		switch (input) {
+		case "a" :
+			addSongToPlaylist(pName);
+		case "r" :
+			removeSongFromPlaylist(pName);
+		}
+	}
+	
+	private void removeSongFromPlaylist(String pName) {
+		System.out.println("Song TITLE of song to remove:");
+		String title = scn.nextLine();
+		checkIfDone(title);
+		System.out.println("Song ARTIST of song to remove:");
+		String artist = scn.nextLine();
+		checkIfDone(artist);
+		System.out.println(libModel.removeSongFromPlaylist(pName, artist, title));
+		start();
+	}
+	
+	private void addSongToPlaylist(String pName) {
+		// maybe print out all songs in library so user can read from them and pick?
+		System.out.println("Song TITLE of song to add:");
+		String title = scn.nextLine();
+		checkIfDone(title);
+		System.out.println("Song ARTIST of song to add:");
+		String artist = scn.nextLine();
+		checkIfDone(artist);
+		System.out.println(libModel.addSongToPlaylist(pName, artist, title));
+		start();
 	}
 	
 	private void checkIfDone(String s) {
